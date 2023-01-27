3248650d891939feda72baa9fbb337d3db38f3f5
Andres Bermudez <andres.bermudez@duke.edu>
2023-01-27 12:54:14 -0500

    final debugging, main function working

--- Classwork/dosing.py
+++ Classwork/dosing.py
@@ -26,29 +26,34 @@ def get_weight_input():
     weight_input = input("Enter weight: ")
     return weight_input
 
-def dose_amount_calc(diagnosis,weight_input):
+def parse_weight(weight_input):
     weight_data = weight_input.split(" ")
     weight = float(weight_data[0])
     units = weight_data[1]
     if units == "lb":
         weight = weight / 2.205
+    return weight
+
+def dose_amount_calc(diagnosis,weight):
     dosages_mg_per_kg = [30, 10, 10, 12]
     dosage_mg_per_kg = dosages_mg_per_kg[diagnosis-1]
     dosage_mg_first_day = weight * dosage_mg_per_kg
-    return weight,dosage_mg_per_kg,dosage_mg_first_day
+    return dosage_mg_per_kg,dosage_mg_first_day
 
 def print_dosage(weight,dosage_mg_per_kg,dosage_mg_first_day):
     print("CORRECT DOSAGE")
     print("For a patient weighing {:.1f} kg,".format(weight))
     print("  the correct dosage is {:.1f} mg the first day"
           .format(dosage_mg_first_day))
+    
 def dose_amount():
     print_dosing_interface()
     dosing_in=dosing_input()
     print_weight_interface()
     weight_in =get_weight_input()
-    dose_info=dose_amount_calc(dosing_in,weight_in)
-    print_dosage(dose_info[1],dose_info[1],dose_info[2])
+    weight=parse_weight(weight_in)
+    dose_info=dose_amount_calc(dosing_in,weight)
+    print_dosage(weight,dose_info[0],dose_info[1])
     
 
 if __name__ == '__main__':
