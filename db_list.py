30c49ae89f6e16db50e939d52b8a47a342e2abb1
Andres Bermudez <andres.bermudez@duke.edu>
2023-02-01 12:21:42 -0500

    Created db_list script that will eventually patient entries

---
+++ db_list.py
@@ -0,0 +1,8 @@
+def create_patient_entry(patient_name,patient_mrn,patient_age):
+    new_patient = [patient_name, patient_mrn, patient_age]
+    return new_patient
+
+
+if __name__=="__main__":
+    main_driver()
+
