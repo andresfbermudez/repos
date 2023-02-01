9a46cff43fbcee4a6c572a73e9828d2edf1990b0
Andres Bermudez <andres.bermudez@duke.edu>
2023-02-01 12:32:41 -0500

    Created function to get a patient entry based on MRN

--- db_list.py
+++ db_list.py
@@ -8,6 +8,16 @@ def main_driver():
     db.append(create_patient_entry("Bob Boyles", 2,45))
     db.append(create_patient_entry("Charlie Chou", 1,52))
     print(db)
+    print("Get patient Ann")
+    found_patient = get_patient_entry(1,db)
+    print(found_patient)
+
+def get_patient_entry(mrn_to_find,db):
+    for patient in db:
+        if patient[1] == mrn_to_find:
+            return patient
+    else:
+        return False
 if __name__=="__main__":
     main_driver()
 
