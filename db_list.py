fcccffed60eb8206d9655d89de5d62bc0a533ff8
Andres Bermudez <andres.bermudez@duke.edu>
2023-02-01 13:06:50 -0500

    Created function that retrieves patient test result based on MRN and test name

--- db_list.py
+++ db_list.py
@@ -8,15 +8,28 @@ def main_driver():
     db.append(create_patient_entry("Bob Boyles", 2,45))
     db.append(create_patient_entry("Charlie Chou", 1,52))
     print(db)
-    print(db)
-    mrn_to_find = 2
-    found_patient = get_patient_entry(mrn_to_find,db)
-    if found_patient is False:
-       print("Patient mrn {} not found".format(mrn_to_find))
-    else:
-       add_test_to_patient(db,mrn_to_find,"HDL",120)
+    add_test_to_patient(db,1,"HDL",120)
+    add_test_to_patient(db,2,"LDL",100)
     
-
+    mrn_to_find = 1
+    test_to_find = "HDL"
+    found_patient, test_result = get_test_result(db,mrn_to_find,test_to_find)
+    print("Patient {} result for {} test is {}".format(found_patient[0],test_to_find,test_result))
+          
+def get_test_result(db,mrn_to_find,test_name):
+    patient = get_patient_entry(mrn_to_find,db)
+    for test in patient[3]:
+        if test[0] == test_name:
+            return patient, test[1]
+        else:
+            return False
+        
+def print_directory(db,room_numbers):
+    for i,patient in enumerate(db):
+        print("Patient {} is in room {}".format(patient[0],room_numbers[i]))
+    for patient,rn in zip(db,room_numbers):
+        print("Patient {} is in room {}".format(patient[0],rn))
+                      
 def get_patient_entry(mrn_to_find,db):
     for patient in db:
         if patient[1] == mrn_to_find:
