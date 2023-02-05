bcbe5488526f42de8ad94a09ea931c02a29839d0
Andres Bermudez <andres.bermudez@duke.edu>
2023-02-05 18:30:27 -0500

    Added unit test to check for slope calculation given two points

--- Classwork/test_ExerciseFeb3.py
+++ Classwork/test_ExerciseFeb3.py
@@ -1,6 +1,12 @@
-def test_find_y_value(slope, intercept, x_input):
+
+
+def test_find_y_value():
+
     from ExerciseFeb3 import find_y_value
     #Arrange
+    slope = 10
+    intercept = 1
+    x_input = 5
     expected = slope*x_input + intercept
     #Act
     answer = find_y_value(x_input)
@@ -8,3 +14,13 @@ def test_find_y_value(slope, intercept, x_input):
     assert answer == expected
 
 
+def test_find_slope():
+    from ExerciseFeb3 import find_slope
+    #Arrange
+    point1 = (2,2)
+    point2 = (4,4)
+    expected = 1
+    #Act
+    answer = find_slope((2,2),(4,4))
+    #Assert
+    assert answer == expected
