aa33775e06d97d11aef55fc368741531e44d159f
Andres Bermudez <andres.bermudez@duke.edu>
2023-02-05 22:28:28 -0500

    Completed Excersive to find point and fixed typos in test, all tests passed

--- Classwork/ExerciseFeb3.py
+++ Classwork/ExerciseFeb3.py
@@ -1,14 +1,18 @@
-def point_finder((x1,y1),(x2,y2),x_input):
-    point1 = (x1,y1)
-    point2 = (x2,y2)
-    slope = find_slope(point1,point2)
+def point_finder(point1, point2, x_input):
+    slope = find_slope(point1, point2)
     intercept = find_intercept(point1, point2, slope)
-    y_value = find_y_value(slope, intercept,x_input)
+    y_value = find_y_value(slope, intercept, x_input)
     return y_value
 
 
 def find_slope(point1, point2):
-    slope = (point2(2) - point1(2))/(point2(1) - point1(1))
+    slope = (point2[1] - point1[1])/(point2[0] - point1[0])
     return slope
 
+def find_intercept(point1, point2, slope):
+    intercept = point1[1] - slope * point1[0]
+    return intercept
 
+def find_y_value(slope, intercept, x_input):
+    y_value = slope * x_input + intercept
+    return y_value
--- Classwork/test_ExerciseFeb3.py
+++ Classwork/test_ExerciseFeb3.py
@@ -1,7 +1,20 @@
 
 
-def test_find_y_value():
+def test_point_finder():
+    from ExerciseFeb3 import point_finder
+    #Arrange
+    point1 = (1,1)
+    point2 = (2,2)
+    x_input = 3
+    expected = 3
+    #Act
+    answer = point_finder(point1, point2, x_input)
+    #Assert
+    assert answer == expected
 
+
+
+def test_find_y_value():
     from ExerciseFeb3 import find_y_value
     #Arrange
     slope = 10
@@ -9,7 +22,7 @@ def test_find_y_value():
     x_input = 5
     expected = slope*x_input + intercept
     #Act
-    answer = find_y_value(x_input)
+    answer = find_y_value(slope, intercept, x_input)
     #Assert
     assert answer == expected
 
@@ -17,21 +30,21 @@ def test_find_y_value():
 def test_find_slope():
     from ExerciseFeb3 import find_slope
     #Arrange
-    point1 = (2,2)
-    point2 = (4,4)
+    point1 = (2, 2)
+    point2 = (4, 4)
     expected = 1
     #Act
-    answer = find_slope((2,2),(4,4))
+    answer = find_slope((2, 2), (4, 4))
     #Assert
     assert answer == expected
 
 def test_find_intercept():
     from ExerciseFeb3 import find_intercept
     #Arrange
-    point1 = (5,5)
-    point2 = (10,15)
+    point1 = (5, 5)
+    point2 = (10, 15)
     expected = -5
     #Act
-    answer = find_intercept((5,5),(10,15))
+    answer = find_intercept((5, 5), (10, 15), 2)
     #Assert
     assert answer == expected
